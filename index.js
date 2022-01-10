73040f1c6bc76776357377d703c64eb0ea9d1682
Shaws <seanrichardparker@gmail.com>
2022-01-10 14:58:47 -0500

    add slider

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
-let sizePref = () => {
-  return window.prompt('How many squares wide to you want your square?');
-} 
-let size = sizePref();
+// let sizePref = () => {
+//   let size = document.getElementById("boxSlider").value;
+//     return size;
+// } 
+
+let slider = document.getElementById("boxSlider");
+let sliderValue = document.getElementById("slideValue");
+sliderValue.innerHTML = slider.value; // Display the default slider value
 
 let container = document.querySelector('#container');
 
-let i = 0;
-while(i < size * size) {
-  let box = document.createElement('div');
-  box.classList.add('box');
-  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-  // container.style.width = `${size}em`
-  box.addEventListener('mouseenter', e => {
-    box.classList.add("gray");
-  });
-  container.appendChild(box);
-  i++;
+let renderGrid = (size) => {
+  let i = 0;
+  while(size * size > i) {
+    let box = document.createElement('div');
+    box.classList.add('box');
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    // container.style.width = `${size}em`
+    box.addEventListener('mouseenter', e => {
+      box.classList.add("gray");
+    });
+    container.appendChild(box);
+    i++;
+  }
 }
+
+renderGrid(slider.value);
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  sliderValue.innerHTML = this.value;
+  renderGrid(slider.value);
+}
+
+
 
 let clear = document.querySelector('#clear');
 
@@ -26,6 +42,6 @@ clear.addEventListener('click', e => {
   box.forEach(e => {
     e.classList.remove('gray');
   })
-
-  size();
+  
+  renderGrid(slider.value);
 })
